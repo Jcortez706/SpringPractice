e7d6b169990bf0b769a3866b499742d19273daf8
Jacob Cortez <jacob.cortez@teamseco.com>
2024-07-19 11:22:47 -0400

    Updated Controller.java

--- src/main/java/com/example/springtest/Controller.java
+++ src/main/java/com/example/springtest/Controller.java
@@ -8,27 +8,29 @@ import static com.example.springtest.Details.Data;
 
 public class Controller {
 
-    @GetMapping("/get") public String home(){
+    @GetMapping("/get")
+    String getData(){
         // Get request displays data
         return "Data " + Data.toString();
     }
 
     @PostMapping("/post")
 
-    String insert(@RequestBody Details ob)
+    String insertData(@RequestBody Details ob)
     {
         // Adds an int to Data
         Data.add(ob.number);
         return "Data Inserted";
     }
 
-    @DeleteMapping("/{id}") String delete(@PathVariable("id") int id){
+    @DeleteMapping("/{id}")
+    String deleteData(@PathVariable("id") int id){
         // Calls delete method on id
         Details.delete(id);
         return "Data Deleted";
     }
     @PutMapping("/put")
-    String put (@RequestBody Details ob){
+    String putData(@RequestBody Details ob){
         // Updates specific number with new number
         Details.put(ob.number, ob.newNumber);
         return "Data Updated";
