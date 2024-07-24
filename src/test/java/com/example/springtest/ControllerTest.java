4a440503ab4df4ca34aee2a4977b64f424232a7f
Jacob Cortez <jacob.cortez@teamseco.com>
2024-07-24 11:54:06 -0400

    Updated tests

--- src/test/java/com/example/springtest/ControllerTest.java
+++ src/test/java/com/example/springtest/ControllerTest.java
@@ -1,22 +1,24 @@
 package com.example.springtest;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.ArgumentMatchers.contains;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,20 +28,24 @@ class TestingWebApplicationTest {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
-    @MockBean
-    private Details details;
+
 
     @Test
     void shouldReturnEmptyDataList() throws Exception {
-        this.mockMvc.perform(get("/get")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform(get("/get"))
+                .andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Data []")));
     }
 
+
+
     @Test
     void shouldReturnPutDataResult() throws Exception{
-        Details detailsTest = new Details(1,2);
-
-        String json = objectMapper.writeValueAsString(detailsTest);
+        Map<String, Integer> testMap = new HashMap<String, Integer>();
+        testMap.put("number", 1);
+        testMap.put("new", 2);
+        String json = objectMapper.writeValueAsString(testMap);
         this.mockMvc.perform(put("/put")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json))
@@ -50,9 +56,9 @@ class TestingWebApplicationTest {
 
     @Test
     void shouldReturnPostDataResult()throws Exception{
-        Details detailsTest = new Details(1);
-
-        String json = objectMapper.writeValueAsString(detailsTest);
+        Map<String, Integer> testMap = new HashMap<String, Integer>();
+        testMap.put("number", 1);
+        String json = objectMapper.writeValueAsString(testMap);
         this.mockMvc.perform(post("/post")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json))
@@ -62,9 +68,10 @@ class TestingWebApplicationTest {
     }
     @Test
     void shouldReturnDeleteDataResult() throws Exception{
-        Details detailsTest = new Details();
-        detailsTest.insertData(1);
-
+        Map<String, Integer> numberMap = new HashMap<String, Integer>();
+        numberMap.put("number", 1);
+        Controller controller = new Controller();
+        controller.insertData(numberMap);
         this.mockMvc.perform(delete("/1"))
                 .andDo(print())
                 .andExpect(status().isOk())
