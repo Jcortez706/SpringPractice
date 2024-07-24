90089c52519bc865ffda44eeda2ffbad8d7cd77d
Jacob Cortez <jacob.cortez@teamseco.com>
2024-07-24 11:38:29 -0400

    Added CrossOrigin annotation.

--- src/main/java/com/example/springtest/Controller.java
+++ src/main/java/com/example/springtest/Controller.java
@@ -2,37 +2,49 @@ package com.example.springtest;
 
 import org.springframework.web.bind.annotation.*;
 
-import static com.example.springtest.Details.Data;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 
 @RestController
 
 public class Controller {
-
+    private final List<Integer> numbers = new ArrayList<>();
+    @CrossOrigin(origins = "http://127.0.0.1:8080")
     @GetMapping("/get")
     String getData(){
         // Get request displays data
-        return "Data " + Data.toString();
+        return "Data " + numbers;
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:8080")
     @PostMapping("/post")
-
-    String insertData(@RequestBody Details ob)
+    String insertData(@RequestBody Map<String, Integer> request)
     {
         // Adds an int to Data
-        Data.add(ob.number);
+        Integer number = request.get("number");
+        numbers.add(number);
         return "Data Inserted";
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:8080")
     @DeleteMapping("/{id}")
     String deleteData(@PathVariable("id") int id){
         // Calls delete method on id
-        Details.delete(id);
+        numbers.remove((Integer) id);
         return "Data Deleted";
     }
+    @CrossOrigin(origins = "http://127.0.0.1:8080")
     @PutMapping("/put")
-    String putData(@RequestBody Details ob){
+    String putData(@RequestBody Map<String, Integer> update){
         // Updates specific number with new number
-        Details.put(ob.number, ob.newNumber);
+        Integer newNumber = update.get("new");
+        Integer numberToReplace = update.get("number");
+        int temp = numbers.indexOf(numberToReplace);
+        if (temp != -1) {
+            numbers.set(temp, newNumber);
+        }
         return "Data Updated";
     }
 }
