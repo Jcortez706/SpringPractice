fcb8eefb6407b441f528754fa6d29a8c5ce93926
Jacob Cortez <jacob.cortez@teamseco.com>
2024-07-26 08:40:18 -0400

    Removed line that saves new data

--- src/main/java/com/example/springtest/LoadDatabase.java
+++ src/main/java/com/example/springtest/LoadDatabase.java
@@ -4,14 +4,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.sql.SQLOutput;
 
 @Configuration
 public class LoadDatabase {
     @Bean
     CommandLineRunner initDatabase(DataRepository repository){
         return args ->{
-            System.out.println("Preloading " + repository.save(new Data(1,12)));
             repository.findAll().forEach(data -> System.out.println("Loaded " + data));
         };
     }
