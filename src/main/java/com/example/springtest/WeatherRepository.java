c099a8c38958cf2919800bee337d60b3cfaccee7
Jacob Cortez <jacob.cortez@teamseco.com>
2024-08-08 15:32:41 -0400

    Created WeatherRepository

--- src/main/java/com/example/springtest/WeatherRepository.java
+++ src/main/java/com/example/springtest/WeatherRepository.java
@@ -1,4 +1,19 @@
 package com.example.springtest;
 
-public interface WeatherRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface WeatherRepository extends JpaRepository<Weather, Integer> {
+    @Query("SELECT w.weatherID FROM Weather w")
+    List<Integer> findAllWeather();
+
+    @Query("SELECT w FROM Weather w WHERE w.date = :date")
+    List<Weather> findWeatherByDate(@Param("date") LocalDate date);
 }
